e526c19d93141d36631f22bf3b18e9c9be484c23
Rui Ventura <rui.ventura@tecnico.ulisboa.pt>
2021-07-26 21:19:09 +0100

    Add final evaluation section

--- src/jl/voronoi_bench.jl
+++ src/jl/voronoi_bench.jl
@@ -12,42 +12,6 @@ randpoints(n=21, lo=nextfloat(1.), hi=prevfloat(2., 2); seed=0xdeadbeef) =
         end
     end
 
-plotxy(dt::DelaunayTriangulation2) =
-    let xs = Float64[], ys = Float64[]
-        foreach(segment.(Ref(dt), edges(dt))) do seg
-            s, t = source(seg), target(seg)
-            push!(xs, x(s)[], x(t)[], NaN)
-            push!(ys, y(s)[], y(t)[], NaN)
-        end
-        xs, ys
-    end
-
-plotxy(vd::VoronoiDiagram2) =
-    let xs = Float64[], ys = Float64[],
-        isbounded(hf) = all(!isnothing, (source(hf), target(hf)))
-        for hf ∈ filter(isbounded, edges(vd))
-            s, t = point.((source(hf), target(hf)))
-            push!(xs, x(s)[], x(t)[], NaN)
-            push!(ys, y(s)[], y(t)[], NaN)
-        end
-        xs, ys
-    end
-
-using Plots
-let ps = randpoints(),
-    cdt = DelaunayTriangulation2([Point2(p...) for p ∈ ps]),
-    vdt = DelaunayTessellation()
-    push!(vdt, [Point(p...) for p ∈ ps])
-
-    plot(plotxy(cdt), lims=(1,2), lab = "CGAL", color = :red)
-    dp = plot!(getplotxy(delaunayedges(vdt))
-             , lab = "VoronoiDelaunay", color = :aqua)
-    plot(plotxy(VoronoiDiagram2(cdt)), lims=(0,3), lab = "CGAL", color = :red)
-    vp = plot!(getplotxy(voronoiedges(vdt))
-             , lab = "VoronoiDelaunay", color = :aqua)
-    plot(dp, vp)
-end
-
 using BenchmarkTools
 import Statistics: std
 printhead(io::IO, s, w = 80; delim = "=") =
@@ -56,8 +20,10 @@ printhead(io::IO, s, w = 80; delim = "=") =
           * " $s "
           * delim^(w÷2 - ceil(Int, length(s) / 2) - 1))
 printhead(args...; kwargs...) = printhead(stdout, args...; kwargs...)
-open(joinpath(@__DIR__, "voronoi-cgal.csv"), "w") do cio
-    open(joinpath(@__DIR__, "voronoi-vdjl.csv"), "w") do vio
+
+cd(joinpath(dirname(@__DIR__)), "data")
+open("voronoi-cgal.csv", "w") do cio
+    open("voronoi-vdjl.csv", "w") do vio
         for n ∈ Int.(exp10.(2:6))
             ps = randpoints(n)
             ct = @benchmark insert!(DelaunayTriangulation2()
---
+++ src/jl/voronoi_plots.jl
@@ -0,0 +1,77 @@
+using VoronoiDelaunay
+include("Voronoi.jl"); using .Voronoi
+
+import RandomNumbers.MersenneTwisters: MT19937
+
+randpoints(n=21, lo=nextfloat(1.), hi=prevfloat(2., 2); seed=0xdeadbeef) =
+    let rng = MT19937(seed)
+        map(1:n) do _
+            x = (rand(rng); rand(rng))
+            y = (rand(rng); rand(rng))
+            lo .+ (hi - lo) .* (x, y)
+        end
+    end
+
+plotxy(dt::DelaunayTriangulation2) =
+    let xs = Float64[], ys = Float64[]
+        foreach(segment.(Ref(dt), edges(dt))) do seg
+            s, t = source(seg), target(seg)
+            push!(xs, x(s)[], x(t)[], NaN)
+            push!(ys, y(s)[], y(t)[], NaN)
+        end
+        xs, ys
+    end
+
+plotxy(vd::VoronoiDiagram2) =
+    let xs = Float64[], ys = Float64[],
+        isbounded(hf) = all(!isnothing, (source(hf), target(hf)))
+        for hf ∈ filter(isbounded, edges(vd))
+            s, t = point.((source(hf), target(hf)))
+            push!(xs, x(s)[], x(t)[], NaN)
+            push!(ys, y(s)[], y(t)[], NaN)
+        end
+        xs, ys
+    end
+
+using Plots; pgfplotsx()
+let ps  = randpoints(),
+    cdt = DelaunayTriangulation2(),
+    vdt = DelaunayTessellation()
+    insert!(cdt, [Point2(p...) for p ∈ ps])
+    push!(vdt,   [Point(p...)  for p ∈ ps])
+
+    plot(plotxy(cdt)
+       , lab = "Voronoi.jl"
+       , lims = (.9, 2.1)
+       , titlefontsize = 10
+       , tickfontfamily = "monospace"
+       , color = :red)
+    plot!(getplotxy(delaunayedges(vdt))
+        , lab = "VoronoiDelaunay.jl"
+        , color = :springgreen
+        , seriesalpha = .8
+        , extra_kwargs = :subplot, legend_columns = -1)
+    title!("Delaunay Triangulation")
+    pl1 = current()
+
+    plot(plotxy(VoronoiDiagram2(cdt))
+       , lab = "Voronoi.jl"
+       , titlefontsize = 10
+       , color = :red)
+    plot!(getplotxy(voronoiedges(vdt))
+        , lab = "VoronoiDelaunay.jl"
+        , color = :springgreen
+        , seriesalpha = .8
+        , extra_kwargs = :subplot, legend_columns = -1)
+    title!("Voronoi Diagram")
+    pl2 = current()
+
+    plot(pl1, pl2
+       , xlabel = raw"$x$"
+       , ylabel = raw"$y$"
+       , size = (700,350)
+       , legend = :topright
+       , legendfonthalign = :left)
+    savefig(joinpath(dirname(@__DIR__), "tikz", "voronoi-plots.tikz"))
+    current()
+end
